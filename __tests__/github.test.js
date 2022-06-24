1ef505a68b0c7e8650ecd0e8cbb4d3687dd1742b
Cole Rossman <colerossman99@gmail.com>
2022-06-24 14:46:15 -0700

    should login and redirect users to api/v1/github/dashboard test created

--- __tests__/github.test.js
+++ __tests__/github.test.js
@@ -4,6 +4,7 @@ const request = require('supertest');
 const app = require('../lib/app');
 
 jest.mock('../lib/services/github.js');
+// it will read the file in services and implements the mock functions in mock folder
 
 describe('github routes', () => {
   beforeEach(() => {
@@ -20,5 +21,21 @@ describe('github routes', () => {
     expect(res.header.location).toMatch(
       /https:\/\/github.com\/login\/oauth\/authorize\?client_id=[\w\d]+&scope=user&redirect_uri=http:\/\/localhost:7890\/api\/v1\/github\/callback/i
     );
+  });
+
+  it('should login and redirect users to /api/v1/github/dashboard', async () => {
+    const res = await request
+      .agent(app)
+      .get('/api/v1/github/callback?code=42')
+      .redirects(1);
+
+    expect(res.body).toEqual({
+      id: expect.any(String),
+      username: 'fake_github_user',
+      email: 'not-real@example.com',
+      avatar: expect.any(String),
+      iat: expect.any(Number),
+      exp: expect.any(Number),
+    });
   });
 });
